15d807c4956e33878e6c139d97260af8ee6ec508
Vivek Anand Singh <17vivekanandsingh@gmail.com>
2026-07-25 12:37:09 +0530

    initialised the project

---
+++ src/test/java/com/example/minicart/MiniCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minicart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
